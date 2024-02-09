7c4b9806663a2614dade4dc2bc92005515c2aa99
soundsbetter <emtsev.sound@gmail.com>
2024-02-09 18:22:31 +0400

    add country_code, zip_code

--- web/apps/weather/models.py
+++ web/apps/weather/models.py
@@ -3,7 +3,7 @@ from django.db.models import JSONField
 
 
 class Country(models.Model):
-
+    country_code = models.CharField(max_length=3, primary_key=True)
     name = models.CharField(max_length=100)
 
 
@@ -11,6 +11,7 @@ class Country(models.Model):
 class City(models.Model):
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    zip_code = models.IntegerField()
 
     def __str__(self):
         return f"{self.name} in {self.country.name}"
